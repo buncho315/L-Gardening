d3d370c1126e0c5e6a55b092aa601060d8372b6b
buncho315 <mzh.hrtk125@gmail.com>
2022-09-04 11:45:44 +0000

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  get 'homes/top'
+  root to: "homes#top"
+  get "/about" => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

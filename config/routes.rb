043c0d4adb837caa11ab6afd274f5882aa00611c
buncho315 <mzh.hrtk125@gmail.com>
2022-09-04 11:18:41 +0000

    devise session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+  devise_for :users, controllers: {
+    sessions: "public/sessions",
+    registrations: "public/registrations"
+  }
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

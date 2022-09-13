646e9a3663eeed4197e8588b33a7a51494e30132
buncho315 <mzh.hrtk125@gmail.com>
2022-09-13 11:38:11 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:passwords], controllers: {
+  devise_for :user, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions",
-
   }
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+
+  namespace :public do
+    resources :users, only: [:show, :edit, :update]
+    resources :books do
+      resources :comments, only: [:create, :index, :destroy]
+      resource :favorites, only: [:create, :destroy]
+      #いいね機能
+    end
+  end
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
+  namespace :admin do
+    root :to =>"homes#top"
+    resources :books, only: [:index]
+  end
+
   root to: "homes#top"
   get "/about" => 'homes#about', as: 'about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
